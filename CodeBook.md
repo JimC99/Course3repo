c809d38a7a7158943030ff9741c7bffb6b478ea1
JimC <jimcarle@hotmail.com>
2019-02-19 20:11:17 -0500

    Updated code and files for course project

--- CodeBook.md
+++ CodeBook.md
@@ -8,18 +8,17 @@ The data were obtained from recordings on 30 subjects (21 'train' and 9 'test') 
 waist-mounted smartphones with embedded inertial sensors. 
 
 The files provided are:
-* X_train.txt and X_test.txt: observational measurements on 561 variables (including mean and std dev of acceleration in each of three ordinal directions).
-* y_train and y_test: activity lables corresponding to the six activities.
+* X_train.txt and X_test.txt: observational measurements on 561 variables.
+* y_train and y_test: activity lables corresponding to six activities.
 * subject_train and subject_test: number keys representing subjects from whom the 'X' measurements were collected.
-* features.txt and activity_labels.txt: names representing 561 observational data types, including the six of interest for this analysis.
+* features.txt and activity_labels.txt: names representing 561 observational data types, including the 66 of interest for this analysis.
 
 ## File manipulations and data transformation
 *Script 'run_analysis.R' extracts individual observations from files 'X_train.txt' and 'X_test.txt', and combines them into a tidy data set 
 that also includes information (columns) on subject and activity, obtained from files 'subject_test.txt', 'subject_train.txt', 'y_test', 'y_train', 
 'features.txt', and 'activity_labels.txt'. 
 
-*Script 'run_analysis.R' also creates a data frame of mean values of each of the 
-six observational variables (mean and std dev of acceleration in each of 3 ordinal directions), summarized by unique subject-activity combination.
+*Script 'run_analysis.R' also creates a data frame of mean values of each of the sixty-six observational variables, summarized by unique subject-activity combination.
 
-*Script 'run_analysis.R' writes both generated data frames to disk as csv files.
+*Script 'run_analysis.R' writes both generated data frames to disk as .csv files, and the summary file also as a .txt file.
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 # run_analysis.R
 # This script implements my solution to the Coursera 'Getting and Cleaning Data'
-# programming assignment course project.
+# programming assignment course project. Updated from my previous submission to
+# include all 66 "mean" and "std" variables, and not just the six 'acceleration' ones. 
 #
 library(dplyr)
 #
@@ -22,16 +23,20 @@ features <-tbl_df(read.table("./UCI HAR Dataset/features.txt",
 activity_labels <-tbl_df(read.table("./UCI HAR Dataset/activity_labels.txt",
                             sep="",stringsAsFactors=F,header=F))
 #
-# Combine the observational ('X') data frames, keeping only the mean and stdev 
-# acceleration columns for x, y, and z directions. Extract column headers for 
-# the observational data from the 'features' data frame.
+# Combine the observational ('X') data frames, select & keep only the 66 'mean' 
+# and 'stdev' labeled columns. Extract column headers for the columns of interest  
+# from the 'features' data frame.
 X_bind <- rbind(X_train, X_test)
-X_bind <- select(X_bind, V1:V6)
-names(X_bind) <- features$V2[1:6]
+X_bind <- select(X_bind, V1:V6,V41:V46,V81:V86,V121:V126,V161:V166,V201:V202,V214:V215,
+                 V227:V228,V240:V241,V253:V254,V266:V271,V345:V350,V424:V429,
+                 V503:V504,V516:V517,V529:V530,V542:V543)
+names(X_bind) <- features$V2[c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,
+                               227:228,240:241,253:254,266:271,345:350,424:429,
+                               503:504,516:517,529:530,542:543)]
 #
-# Remove problematic characters from column names.
+# Make the column (variable) names more descriptive by removing/replacing unusual characters.
 names(X_bind) <- gsub("[()]", "", names(X_bind))
-names(X_bind) <- gsub("-", "", names(X_bind))
+names(X_bind) <- gsub("-", ".", names(X_bind))
 #
 # Combine the activity label ('y') data frames, and rename the activity label 
 # column as 'label'.
@@ -47,29 +52,16 @@ names(subject_bind) <- "subject"
 tablbind1 <- cbind(subject_bind, y_bind, X_bind)
 names(activity_labels) <- c("label", "activity")
 # 
-# Add a column of descriptive activity names corresponding to the 'label' values.
-tablbind2 <- merge(tablbind1, activity_labels, by = "label")
+# Add a column of descriptive activity names corresponding to the 'label' values, then
+# delete the now-redundant 'label' column.
+tidy_data <- merge(tablbind1, activity_labels, by = "label")
+tidy_data <- select(tidy_data, subject, activity, tBodyAcc.mean.X:fBodyBodyGyroJerkMag.std)
 #
-# Rename the observational data fields with more descriptive variable names.
-colnames(tablbind2)[colnames(tablbind2)=="tBodyAccmeanX"] <- "x_direction_mean_acceleration"
-colnames(tablbind2)[colnames(tablbind2)=="tBodyAccmeanY"] <- "y_direction_mean_acceleration"
-colnames(tablbind2)[colnames(tablbind2)=="tBodyAccmeanZ"] <- "z_direction_mean_acceleration"
-colnames(tablbind2)[colnames(tablbind2)=="tBodyAccstdX"] <- "x_direction_stddev_acceleration"
-colnames(tablbind2)[colnames(tablbind2)=="tBodyAccstdY"] <- "y_direction_stddev_acceleration"
-colnames(tablbind2)[colnames(tablbind2)=="tBodyAccstdZ"] <- "z_direction_stddev_acceleration"
-#
-# Create a data frame that omits the now-extraneous 'label' column. Write the result to disk
-# as a csv file.
-tidy_data <- select(tablbind2, subject, activity, x_direction_mean_acceleration, 
-                    y_direction_mean_acceleration, z_direction_mean_acceleration, 
-                    x_direction_stddev_acceleration, y_direction_stddev_acceleration, 
-                    z_direction_stddev_acceleration)
 write.csv(tidy_data, "tidy_data_table.csv", row.names=FALSE)
 #
-# Create a data frame of mean values of each of the six observational variables summarized by 
+# Create a data frame of mean values of each of the 66 observational variables summarized by 
 # unique subject-activity combination. Write the result to disk as a csv file.
 summ_tabl <- tidy_data %>% group_by(subject, activity) %>% 
-                     summarise_at(vars("x_direction_mean_acceleration", "y_direction_mean_acceleration", 
-                     "z_direction_mean_acceleration", "x_direction_stddev_acceleration", 
-                     "y_direction_stddev_acceleration", "z_direction_stddev_acceleration"), mean)
-write.csv(summ_tabl, "summmary_mean_table.csv", , row.names=FALSE)
+                     summarise_at(vars("tBodyAcc.mean.X":"fBodyBodyGyroJerkMag.std"), mean)
+write.csv(summ_tabl, "summmary_mean_table.csv", row.names=FALSE)
+write.table(summ_tabl, "summmary_mean_table.txt", row.names=FALSE)
